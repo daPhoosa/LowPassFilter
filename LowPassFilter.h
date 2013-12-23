19eb6049cc80a89668854f0bd2fed171d424c278
Phillip Schmidt <pjs.schmidt@gmail.com>
2013-12-23 14:33:38 -0600

    Update LowPassFilter.h

--- LowPassFilter.h
+++ LowPassFilter.h
@@ -1,25 +1,47 @@
 /*
-  MedianFilter.h - Median Filter for the Arduino platform.
-  Copyright (c) 2012 Phillip Schmidt.  All right reserved.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+  LowPassFilter.cpp - Exponential Decay Filter for the Arduino platform.
+  Copyright (c) 2013 Phillip Schmidt.  All right reserved.
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 /* 
-	!!! All data must be type INT.  !!!
- */
+        !!! All data must be type INT.  !!!
+        
+        Note: This library is intended to run under the Arduino dev platform
+        
+        Usage:
+        #include <LowPassFilter.h>  // library must be in ..\Arduino\libraries\LowPassFilter
+        
+        LowPassFilter filter1(2, 100);  // create filter object with a filter strength of 2
+                                        // and seeded to start at 100 (seed only needs to be a rough guess)
+        
+        filteredData = filter1.in(rawData);// new data is passed to the object and
+                                        // the oject return the current filtered value
+                                        
+        filteredData = filter1.out();        // use the 'out()' function to get the latest result
+                                        // without submitting new data.
+                                        
+        TIPS:
+        -create a filter oject for each uniquie data stream
+        -The filter strength must be an integer
+        -The filter is more effective, but introduces more delay with higher filter strength values
+                (the "smootheness" and delay double for each increase of filter strength)
+        -A filter strength of '0' will have no effect (filteredData = rawData)
+        -Avoid using a larger filter strength than needed as incoming data precision may suffer
+*/
 
 #ifndef LowPassFilter_h
 #define LowPassFilter_h
