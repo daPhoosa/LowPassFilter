95483bebb141831d2295b730f34a7f9e91f34f39
Phillip Schmidt <pjs.schmidt@gmail.com>
2015-01-21 01:44:35 -0600

    Reduce Doc

--- LowPassFilter.cpp
+++ LowPassFilter.cpp
@@ -2,22 +2,7 @@
   LowPassFilter.cpp - Exponential Decay Filter for the Arduino platform.
   Copyright (c) 2013 Phillip Schmidt.  All right reserved.
  
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
  
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-/* 
 	!!! All data must be type INT.  !!!
 	
 	Note: This library is intended to run under the Arduino dev platform
